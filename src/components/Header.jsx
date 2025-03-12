bdf39aef4201d49d9e3f477cb742c8dc531b5578
ubieeel <ultimacontagabi@outlook.com>
2025-03-12 00:54:05 -0300

    Commit de salvamento inicial do projeto

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header(){
+    return (
+        <div>
+            <h1 className="text-3xl font-bold content-center text-center my-auto">Iniciar programacao!</h1>
+        </div>
+    )
+}
+
