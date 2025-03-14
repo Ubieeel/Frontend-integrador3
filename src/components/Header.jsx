4d9ade8dfee83b07389f11e688c5059a20fc3429
gabriel <gabriel.domiciano@aluno.ifsp.edu.br>
2025-03-14 17:53:00 -0300

    criando a header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
 
 export default function Header(){
     return (
-        <div>
-            <h1 className="text-3xl font-bold content-center text-center my-auto">Iniciar programacao!</h1>
+        <body className='bg-black text-white'>
+
+        <nav className="flex items-center justify-between p-4">
+        <div className="flex items-center">
+            <img className="h-20 w-20 mr-2"  src='./img-header/vivant-logo.png' /> 
         </div>
+        <div className='flex items-center space-x-8'>
+            <a className='text-lg' href='#'>Home</a>
+      
+            <a className='text-lg' href='#'>Vinhos</a>
+       
+            <a className='text-lg' href='#'>Sobre nós</a>
+        
+            <a className='text-lg' href='#'>Contato</a>
+
+            <a className='text-lg' href='#'>
+                <i className='fas fa-shopping-cart'>
+                </i>
+            </a>
+            <a className='text-lg' href='#'>
+            <AntDesign name="shoppingcart" size={24} color="black" />
+            </a>
+        </div>
+        </nav>
+        </body>
     )
 }
 
